176eadc93cc05172570eb808cae41faf6809e6b2
Pedro Parente <pedrofulfaro@gmail.com>
2022-06-28 18:33:46 -0300

    header fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,3 @@
-// alert(1)
-
-// const request = obj => {
-//     return new Promise((resolve, reject) => {
-//       const xhr = new XMLHttpRequest();
-//       xhr.open(obj.method, obj.url, true);
-//       xhr.send();
-
-//       xhr.addEventListener('load', () => {
-//         if(xhr.status >= 200 && xhr.status < 300) {
-//           resolve(xhr.responseText);
-//         } else {
-//           reject(xhr.statusText);
-//         }
-//       });
-//     });
-//   };
-
-//   document.addEventListener('click', e => {
-//     const el = e.target;
-//     const tag = el.tagName.toLowerCase();
-
-//     if (tag === 'a') {
-//       e.preventDefault();
-//       carregaPagina(el);
-//     }
-//   });
-
-//   async function carregaPagina(el) {
-//     const href = el.getAttribute('href');
-
-//     const objConfig = {
-//       method: 'GET',
-//       url: href
-//     };
-
-//     try {
-//       const response = await request(objConfig);
-//       carregaResultado(response);
-//     } catch(e) {
-//       console.log(e);
-//     }
-//   }
-
-//   function carregaResultado(response) {
-//     const resultado = document.querySelector('.resultado');
-//     resultado.innerHTML = response;
-//   }
-
 
 // // Modal
 
@@ -82,25 +33,21 @@ document.addEventListener("click", (e) => {
 
 // Fixed Header
 
-let header = document.querySelector(".header");
+// let header = document.querySelector(".header");
 
-header.style.paddingTop = "20px";
 
-jQuery(document).scroll(function () {
-    if (jQuery(this).scrollTop() > 90) {
-        jQuery('.header').addClass('header-fixed');
-        jQuery('.wrapper').removeClass('py-4');
-        jQuery('.wrapper').addClass('py-2');
-
-        header.style.paddingTop = "0px";
-    }
-    else {
-        jQuery('.header').removeClass('header-fixed');
-        jQuery('.wrapper').removeClass('py-2');
-        jQuery('.wrapper').addClass('py-4');
-        header.style.paddingTop = "20px";
-    }
-});
+// jQuery(document).scroll(function () {
+//     if (jQuery(this).scrollTop() > 90) {
+//         jQuery('.header').addClass('header-fixed');
+//         jQuery('.wrapper').removeClass('py-4');
+//         jQuery('.wrapper').addClass('py-2');
+//     }
+//     else {
+//         jQuery('.header').removeClass('header-fixed');
+//         jQuery('.wrapper').removeClass('py-2');
+//         jQuery('.wrapper').addClass('py-4');
+//     }
+// });
 
 // AOS
 
